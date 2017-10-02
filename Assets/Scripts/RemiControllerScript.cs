4d7749af22e6a0c51969efbe8cab34b8908bfa90
Michel <michel@imthesolution.nl>
2017-10-02 21:17:07 +0200

    Michel's Scene toegevoegd

---
+++ Assets/Scripts/RemiControllerScript.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemiControllerScript : MonoBehaviour
+{
+
+    public float maxSpeed = 1f;
+    bool facingRight = true;
+
+    Animator anim;
+
+    bool grounded = false;
+    public Transform groundCheck;
+    float groundRadius = 0.2f;
+    public LayerMask whatIsGround;
+
+    public float jumpForce = 700f;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        var rigidBody2D = GetComponent<Rigidbody2D>();
+
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        anim.SetBool("Ground", grounded);
+
+        anim.SetFloat("vSpeed", rigidBody2D.velocity.y);
+
+
+        float move = Input.GetAxis("Horizontal");
+
+        anim.SetFloat("Speed", Mathf.Abs(move));
+
+        rigidBody2D.velocity = new Vector2(move * maxSpeed, rigidBody2D.velocity.y);
+
+        if (move > 0 && !facingRight)
+            Flip();
+        else if (move < 0 && facingRight)
+            Flip();
+    }
+
+    void Update()
+    {
+        var rigidBody2D = GetComponent<Rigidbody2D>();
+
+        if (grounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetBool("Ground", false);
+            rigidBody2D.AddForce(new Vector2(0, jumpForce));
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
