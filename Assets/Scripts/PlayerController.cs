5c66acbcf87d7dd9576275b57881a010ea647340
Rene Buunk <renebuunk1996@gmail.com>
2017-10-02 21:13:55 +0200

    Tutorial added player movement except jumping

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody2D rigidBody;
+    SpriteRenderer renderer;
+    Animator animator;
+
+    bool facingRight = true;
+
+    public float MaxSpeed;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody2D>();
+        renderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float move = Input.GetAxisRaw("Horizontal");
+
+        if(move > 0  && !facingRight)
+        {
+            Flip();
+        }
+        else if(move < 0 && facingRight)
+        {
+            Flip();
+        }
+
+        animator.SetFloat("MoveSpeed", Mathf.Abs(move));
+        rigidBody.velocity = new Vector2(move * MaxSpeed, rigidBody.velocity.y);
+	}
+
+    private void Flip()
+    {
+        facingRight = !facingRight;
+        renderer.flipX = !renderer.flipX;
+    }
+}
